f8208c543b744cd9476c21b18f5827e632ca8d8e
pmz-q <sundeul.dona.kim@gmail.com>
2024-01-10 17:13:12 +0900

    edit: auto-arima code refactoring

--- auto_train.py
+++ auto_train.py
@@ -5,17 +5,17 @@ from train_arima import main as arima_main
 from train_patchtst import main as tst_main
 
 
+TYPE = "tst" # ["arima", "nn", "tst"]
+AUTO_MAPPER = {
+  "arima":arima_main,
+  "nn": nn_main,
+  "tst": tst_main
+}
+
 def main(config_type, config):
-  run = {
-    "arima":arima_main,
-    "nn": nn_main,
-    "tst": tst_main
-  }
-  run[config_type](config)
+  AUTO_MAPPER[config_type](config)
 
 if __name__ == "__main__":
-  TYPE="tst" # ["arima", "nn", "tst"] 
-
   list_of_configs = get_list_of_auto_configs(config_type=TYPE)
   for idx, cfg_file in enumerate(list_of_configs):
     parser = argparse.ArgumentParser(description="second-auto-train", add_help=True)
