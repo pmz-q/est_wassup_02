7e95262948503c1c4f85ad25ea0c763df0ab053e
pmz-q <sundeul.dona.kim@gmail.com>
2024-01-11 13:51:46 +0900

    fixed: arima scaler error

--- train_arima.py
+++ train_arima.py
@@ -15,13 +15,14 @@ def main(config):
     # get preprocessed csv files
     input_data = config.get("input_data")
     df_train = pd.read_csv(input_data.get("train_csv"), index_col=0)
-    df_test = pd.read_csv(input_data.get("test_csv"), index_col=0)
     
     # applying test size
-    df_test = df_test.iloc[-config.get("test_size"):]
+    test_size = config.get("test_size")
+    trn_ds = df_train.iloc[:-test_size]
+    tst_ds = df_train.iloc[-test_size:].copy()
     # change index data type
-    df_train.index = pd.to_datetime(df_train.index)
-    df_test.index = pd.to_datetime(df_test.index)
+    trn_ds.index = pd.to_datetime(trn_ds.index)
+    tst_ds.index = pd.to_datetime(tst_ds.index)
 
     # arima params
     arima_params = config.get("arima_params")
@@ -29,10 +30,9 @@ def main(config):
     # ARIMA or SARIMA
     arima_model = config.get("arima_model")
 
-    pred, tst_ds = arima(df_train, df_test, arima_model, arima_params)
-    tst_ds= pd.DataFrame(tst_ds)
-    pred= pd.DataFrame(pred)
-
+    pred = arima(trn_ds, test_size, tst_ds.index[0], tst_ds.index[-1], arima_model, arima_params)
+    pred = pd.DataFrame({"prediction": pred.values}, index=tst_ds.index)
+    
     # inverse scaling
     y_scaler = None
     try:
@@ -41,8 +41,8 @@ def main(config):
         pass
     
     if y_scaler != None:
-        tst_ds["target"] = y_scaler.inverse_transform(tst_ds["target"].values.reshape(-1, 1))
-        pred["predicted_mean"] = y_scaler.inverse_transform(pred["predicted_mean"].values.reshape(-1, 1))
+        tst_ds["target"] = y_scaler.inverse_transform(tst_ds)
+        pred["prediction"] = y_scaler.inverse_transform(pred)
 
     # Save plot
     output_data = config.get("output_data")
@@ -50,6 +50,7 @@ def main(config):
     pred_save_path = output_data.get("pred_save_path")
     actual_save_path = output_data.get("actual_save_path")
     root_dir = create_path_if_not_exists(output_data.get("root_dir"), remove_filename=False)
+    
     pred.to_csv(combine_paths(root_dir, pred_save_path))
     tst_ds.to_csv(combine_paths(root_dir, actual_save_path))
 
@@ -58,7 +59,7 @@ def main(config):
     plt.figure(figsize=(20, 10))
     plt.plot(tst_ds.index, tst_ds.values, pred.index, pred.values)
     plt.title(
-        f"{arima_model}, MAPE:{mape(pred.values,tst_ds['target'].values):.4f}, MAE:{mae(pred.values,tst_ds['target'].values):.4f}, R2:{r2_score(pred.values,tst_ds['target'].values):.4f}"
+        f"{arima_model}, MAPE:{mape(pred['prediction'].values,tst_ds['target'].values):.4f}, MAE:{mae(pred['prediction'].values,tst_ds['target'].values):.4f}, R2:{r2_score(pred['prediction'].values,tst_ds['target'].values):.4f}"
     )
     plt.savefig(plot_img_path)  # save as png
 
