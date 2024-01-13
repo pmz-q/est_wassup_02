e1086c641f3999a178da587ca71900f2e9cc16a4
pmz-q <sundeul.dona.kim@gmail.com>
2024-01-13 20:31:28 +0900

    mape edited

--- core/metrics/mape.py
+++ core/metrics/mape.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def mape(y_pred, y_true): # mape = mean absolute percentage error
-  return (np.abs(y_pred - y_true)/y_true).mean() * 100
+  return (np.abs(y_pred - y_true)/(y_true+np.e)).mean() * 100
